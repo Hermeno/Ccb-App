6ef3c8c144774628de85144a16a15af3c1e5db37
Hermeno dev <herminiomacamo6@gmail.com>
2025-03-10 21:55:09 +0200

    Adicionado a pagina ddespesas pra exibir e cadastrar

---
+++ app/despesas.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import  { View, Text, Button } from 'react-native';
+
+
+export default function Despesas(){
+    return(
+      <View>
+        <Text></Text>
+      </View>   
+    )
+}
